71559f703e5c1ca8e46a9c5a7de867386df8afa5
Pratheek161001 <pratheeksk70@gmail.com>
2023-11-22 22:26:41 +0530

    page 5 done with animation

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ let tl = gsap.timeline({
       scrub: 1, 
       },
 });
-
 tl.to("#img3",
 { 
     bottom:"-7vh"
@@ -19,3 +18,66 @@ tl.to("#img3",
 { 
     marginBottom:"-25vh"
 })
+
+let tl2 = gsap.timeline({
+    scrollTrigger:{
+      trigger: ".part5",
+      start: "40% 50%", 
+      end: "170% 50%",
+      pin: true,
+      scrub:3, 
+      },
+});
+tl2.to("#scroll-1",
+{ 
+    bottom:"50vh"
+})
+.to("#scroll-1",
+{ 
+    opacity:0,
+},)
+.to("#scroll-2",
+{ 
+    opacity:1,
+},'one')
+.to(".phone-img",
+{ 
+    x:"-20vw",
+},'one')
+
+.to("#scroll-2",
+{ 
+    bottom:"50vh"
+})
+.to("#scroll-2",
+{ 
+    opacity:0,
+},)
+.to("#scroll-3",
+{ 
+    opacity:1,
+},'two')
+.to(".phone-img",
+{ 
+    x:"-40vw",
+},'two')
+
+.to("#scroll-3",
+{ 
+    bottom:"50vh"
+})
+.to("#scroll-3",
+{ 
+    opacity:0,
+},)
+.to("#scroll-4",
+{ 
+    opacity:1,
+},'three')
+.to(".phone-img",
+{ 
+    x:"-60vw",
+},'three')
+
+
+
